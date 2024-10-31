d56d43d0449ef9710b67fa7452d3683a0be53364
AlifNaufaldo <muhammadalifnaufaldo@gmail.com>
2024-10-31 14:58:04 +0700

    fix error

--- iProject/src/api/ServerApi.jsx
+++ iProject/src/api/ServerApi.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const baseURL = 'https://p2.alifnaufaldo.online/';
+// const baseURL = 'https://p2.alifnaufaldo.online/';
 // const baseURL = "http://localhost:5173/"
+const baseURL = "http://localhost:3000/"
 
 const serverAPI = axios.create({baseURL});
 
